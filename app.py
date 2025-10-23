84cee99afe733bc0b9f69f2aa1b374d6df18f3af
cvergaraa <159863822+cvergaraa@users.noreply.github.com>
2025-10-23 10:24:02 -0500

    Rename MQTT client and update publish topic

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def on_message(client, userdata, message):
 
 broker="broker.mqttdashboard.com"
 port=1883
-client1= paho.Client("GIT-HUBC")
+client1= paho.Client("Cliente_cami")
 client1.on_message = on_message
 
 
@@ -75,7 +75,7 @@ if result:
         client1.on_publish = on_publish                            
         client1.connect(broker,port)  
         message =json.dumps({"Act1":result.get("GET_TEXT").strip()})
-        ret= client1.publish("voice_ctrl_ca", message)
+        ret= client1.publish("mensaje_cami", message)
 
     
     try:
